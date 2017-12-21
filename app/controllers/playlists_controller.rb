ce9df5d2af219ffdad3c6e9d97ebdf53feb383e1
jimmy ibanez <jimmy.ibanez@outlook.com>
2017-12-21 17:15:51 -0300

    Authentication, creation and update of playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,5 @@
 class PlaylistsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @playlists = Playlist.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'spotify/search'
 
   resources :tracks
   resources :playlists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'tracks#index'
 end
